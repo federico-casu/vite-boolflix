8ebc733c516bbc7989f8e32fbe54adbf08087cee
federico-casu <federico.casu1101@gmail.com>
2024-02-13 13:56:16 +0100

    feat: added search bar and icon

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,22 @@
 
 <script>
     // Js componente
+
+    import { store } from '../store';
+
     export default {
         name: 'AppHeader',
         data() {
             return {
-                menu: [ 'TV Shows', 'Movies', 'Recently Added', 'My List' ]
+                store,
+                menu: [ 'TV Shows', 'Movies', 'Recently Added', 'My List' ],
+                searchBarDisplay: false
             }
         },
         methods: {
-            
+            changeDisplay() {
+                this.searchBarDisplay ? this.searchBarDisplay = false : this.searchBarDisplay = true
+            }
         }
     }
 </script>
@@ -19,15 +26,25 @@
     <!-- html componente -->
     <header>
         <div class="container">
-            <figure>
-                <img src="../assets/img/logo.png" alt="Boolflix">
-            </figure>
+            <div id="logo-and-menu">
+                <!-- Logo -->
+                <figure>
+                    <img src="../assets/img/logo.png" alt="Boolflix">
+                </figure>
+    
+                <!-- Menu -->
+                <nav>
+                    <ul>
+                        <li v-for="(item, index) in menu" >{{ item }}</li>
+                    </ul>
+                </nav>
+            </div>
 
-            <nav>
-                <ul>
-                    <li v-for="(item, index) in menu" >{{ item }}</li>
-                </ul>
-            </nav>
+            <!-- Search bar -->
+            <div id="search">
+                <input v-show="searchBarDisplay" v-model="store.searchText" type="text" placeholder="Search..">
+                <i @click="changeDisplay" class="fa-solid fa-magnifying-glass"></i>
+            </div>
         </div>
     </header>
     
@@ -50,28 +67,70 @@
             height: 100%;
 
             @include d-flex-align-center;
+            justify-content: space-between;
 
-            figure {
-                max-height: 100%;
-                width: fit-content;
+            #logo-and-menu {
 
-                > img {
-                    width: 80%;
+                @include d-flex-align-center;
+
+                figure {
+                    max-height: 100%;
+                    width: fit-content;
+    
+                    > img {
+                        width: 80%;
+                    }
                 }
-            }
-
-            nav {
-                ul {
-                    @include d-flex-align-center;
-
-                    gap: 1.5rem;
-
-                    li {
-                        list-style-type: none;
-                        font-size: 1.3rem;
+    
+                nav {
+                    ul {
+                        @include d-flex-align-center;
+    
+                        gap: 1.5rem;
+    
+                        li {
+                            list-style-type: none;
+                            font-size: 1.3rem;
+                        }
                     }
                 }
             }
+
+
+            #search {
+                height: 100%;
+                width: 30%;
+
+                @include d-flex-align-center;
+                justify-content: flex-end;
+                gap: 1rem;
+
+                input {
+                    height: 50%;
+                    width: 100%;
+                    border: 0;
+                    background-color: transparent;
+                    border-bottom: 1px solid white;
+                    border-radius: 10px;
+                    padding: 10px;
+                    color: white;
+                }
+
+                i {
+                    height: 50%;
+                    aspect-ratio: 1;
+
+                    @include d-flex-align-center;
+                    justify-content: center;
+                    font-size: 1.5rem;
+                    // color: white;
+                }
+
+                margin-right: 1rem;
+            }
+
+
+            
         }
     
     }
--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiKey: 'api_key=444cf24f305e693301ecd3a903019cce'
+    apiKey: 'api_key=444cf24f305e693301ecd3a903019cce',
+    searchText: ''
 })
