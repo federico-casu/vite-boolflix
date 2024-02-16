03df813bf1d37af1bb044d111ad5a121bbd10b47
federico-casu <federico.casu1101@gmail.com>
2024-02-16 15:30:28 +0100

    feat: added feature to flip a card by click to see all details

--- src/components/main/Card.vue
+++ src/components/main/Card.vue
@@ -13,7 +13,8 @@
         },
         data() {
             return {
-                store
+                store,
+                flipped: false
             }
         },
         methods: {
@@ -34,6 +35,9 @@
                     default:
                         return str
                 } 
+            },
+            toggleCard() {
+                this.flipped = !this.flipped;
             }
         }
     }
@@ -42,68 +46,73 @@
 
 <template>
     <!-- html componente -->
-    <div class="card">
-        <!-- immagine di copertina -->
-        <figure>
-            <img :src="propsElement.poster_path != null ? `${store.apiUrlImages}${propsElement.poster_path}` : `${`../../src/assets/img/cover_image_unavailable.png`}`" :alt="propsElement.title">
-        </figure>
+    <div @click="toggleCard()" @mouseleave="flipped ? toggleCard() : ''" class="card">
+        <transition name="flip">
+            <div :key="flipped">
+                <!-- immagine di copertina -->
+                <figure v-show="!flipped" class="card-cover">
+                    <img :src="propsElement.poster_path != null ? `${store.apiUrlImages}${propsElement.poster_path}` : `${`../../src/assets/img/cover_image_unavailable.png`}`" :alt="propsElement.title">
+                </figure>
+        
+                <!-- Info -->
+                <div v-show="flipped" class="card-info">
+                    <!-- Titolo -->
+                    <h4>{{ propsElement.title ? propsElement.title : propsElement.name }}</h4>
+        
+                    <!-- Titolo originale -->
+                    <p v-if="propsElement.title != propsElement.original_title || propsElement.name != propsElement.original_name">
+                        {{ propsElement.original_title ? propsElement.original_title : propsElement.original_name }}
+                    </p>
+        
+                    <!-- Paese d'origine o Lingua -->
+                    <figure id="lang-flags">
+                        <img 
+                        v-if="propsElement.origin_country && propsElement.origin_country.length != 0" 
+                        v-for="(item, index) in propsElement.origin_country" 
+                        :key="index" 
+                        width="24" 
+                        :src="`https://flagsapi.com/${item}/shiny/24.png`" 
+                        :alt="item">
+                        <img 
+                        v-else 
+                        width="24" 
+                        :src="`https://flagsapi.com/${this.checkLang(propsElement.original_language).toUpperCase()}/shiny/24.png`" 
+                        :alt="propsElement.original_language">
+                    </figure>
+        
+                    <!-- Voto -->
+                    <span>
+                        <span
+                        id="stars" 
+                        v-for="items in Math.round((propsElement.vote_average) / 2)" >
+                            <i class="fa-solid fa-star"></i>
+                        </span>
+                        <span
+                        v-for="items in (5 - Math.round((propsElement.vote_average) / 2))">
+                            <i class="fa-regular fa-star"></i>
+                        </span>
+                    </span>
+        
+                    <!-- Cast -->
+                    <span v-if="propsElement.cast && propsElement.cast.length != 0">
+                        <strong>Cast: </strong>
+                        <span v-for="(actor, index) in propsElement.cast" :key="index">{{ actor.name }}<span v-if="index != propsElement.cast.length-1">, </span></span>
+                    </span>
+        
+                    <!-- Generi -->
+                    <span v-if="propsElement.genres && propsElement.genres.length != 0">
+                        <strong>Genres: </strong>
+                        <span v-for="(genre, index) in propsElement.genres" :key="index">{{ genre }}<span v-if="index != propsElement.genres.length-1">, </span></span>
+                    </span>
+        
+                    <!-- Overview -->
+                    <p v-if="propsElement.overview" id="overview">
+                       <strong>Overview: </strong> {{ propsElement.overview }}
+                    </p>
+                </div>
+            </div>
+        </transition>
 
-        <!-- Info -->
-        <div class="card-info">
-            <!-- Titolo -->
-            <h4>{{ propsElement.title ? propsElement.title : propsElement.name }}</h4>
-
-            <!-- Titolo originale -->
-            <p v-if="propsElement.title != propsElement.original_title || propsElement.name != propsElement.original_name">
-                {{ propsElement.original_title ? propsElement.original_title : propsElement.original_name }}
-            </p>
-
-            <!-- Paese d'origine o Lingua -->
-            <figure id="lang-flags">
-                <img 
-                v-if="propsElement.origin_country && propsElement.origin_country.length != 0" 
-                v-for="(item, index) in propsElement.origin_country" 
-                :key="index" 
-                width="24" 
-                :src="`https://flagsapi.com/${item}/shiny/24.png`" 
-                :alt="item">
-                <img 
-                v-else 
-                width="24" 
-                :src="`https://flagsapi.com/${this.checkLang(propsElement.original_language).toUpperCase()}/shiny/24.png`" 
-                :alt="propsElement.original_language">
-            </figure>
-
-            <!-- Voto -->
-            <span>
-                <span
-                id="stars" 
-                v-for="items in Math.round((propsElement.vote_average) / 2)" >
-                    <i class="fa-solid fa-star"></i>
-                </span>
-                <span
-                v-for="items in (5 - Math.round((propsElement.vote_average) / 2))">
-                    <i class="fa-regular fa-star"></i>
-                </span>
-            </span>
-
-            <!-- Cast -->
-            <span v-if="propsElement.cast && propsElement.cast.length != 0">
-                <strong>Cast: </strong>
-                <span v-for="(actor, index) in propsElement.cast" :key="index">{{ actor.name }}<span v-if="index != propsElement.cast.length-1">, </span></span>
-            </span>
-
-            <!-- Generi -->
-            <span v-if="propsElement.genres && propsElement.genres.length != 0">
-                <strong>Genres: </strong>
-                <span v-for="(genre, index) in propsElement.genres" :key="index">{{ genre }}<span v-if="index != propsElement.genres.length-1">, </span></span>
-            </span>
-
-            <!-- Overview -->
-            <p v-if="propsElement.overview" id="overview">
-               <strong>Overview: </strong> {{ propsElement.overview }}
-            </p>
-        </div>
     </div>
     
 </template>
@@ -115,6 +124,18 @@
     @use '../../assets/styles/general.scss' as *;
     @use '../../assets/styles/partials/mixins' as *;
 
+    .flip-enter-active {
+        transition: all 0.4s ease;
+    }
+  
+    .flip-leave-active {
+        display: none;
+    }
+  
+    .flip-enter-from, .flip-leave-to {
+        transform: rotateY(180deg);
+        opacity: 0; 
+    }
     .card {
         flex-basis: calc(100% / 5 - (40px / 5));
         max-width: calc(100% / 5 - (40px / 5));
@@ -124,16 +145,22 @@
         // padding: 0.8rem;
         border: 1px solid pink;
         min-height: 250px;
+        cursor: pointer;
 
         position: relative;
 
         justify-content: center;
 
+
+        &:hover {
+            transform: scale(1.1);
+            z-index: 1;
+        }
         &:hover .card-info {
             @include d-flex;
         }
 
-        > figure {
+        > div, .card-cover {
             width: 100%;
             height: 100%;
             img {
@@ -156,7 +183,7 @@
             top: 0;
             left: 0;
             right: 0;
-            background-color: rgba($color: #121212, $alpha: 0.5);
+            background-color: rgba($color: #303030, $alpha: 1);
 
             #lang-flags {
                 @include d-flex;
